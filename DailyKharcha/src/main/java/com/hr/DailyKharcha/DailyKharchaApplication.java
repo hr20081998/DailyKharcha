9a9de3b854de7200ccebbfd1eb1ad6d562f8713d
hr20081998 <hemantsinghrathoureofficial@gmail.com>
2024-05-10 16:57:25 +0530

    Add files via upload

---
+++ DailyKharcha/src/main/java/com/hr/DailyKharcha/DailyKharchaApplication.java
@@ -0,0 +1,13 @@
+package com.hr.DailyKharcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyKharchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyKharchaApplication.class, args);
+	}
+
+}
---
+++ DailyKharcha/src/test/java/com/hr/DailyKharcha/DailyKharchaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hr.DailyKharcha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyKharchaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
